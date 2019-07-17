9463f1ef6e57dfea5dcccbdc8c77cde211a57313
Mohammed Asslahi <asslahi37@gmail.com>
2019-07-17 20:16:08 +0300

    added ErrorBoundry func to help us with catching erros

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+import ErrorBoundry from '../components/Errorboundry'
 
 class App extends Component {
   constructor() {
@@ -35,7 +36,9 @@ class App extends Component {
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
